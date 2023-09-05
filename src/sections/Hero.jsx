20483207ec0514c735bba977673d8dbbdc4962db
mohammad khakshoor <mkhbeats@gmail.com>
2023-09-05 21:31:52 +0330

    FIX: fix the z-index of heading title in the header

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
             <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
                 <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
                 <h1 className="mt-10 text-8xl font-bold font-palanquin max-sm:text-[72px] max-sm:leading-[82px]">
-                    <span className=" xl:bg-white xl:whitespace-nowrap relative z-2 pr-10">
+                    <span className=" xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
                         The New Arrival
                     </span>
                     <br /> <span className=" text-coral-red mt-3 inline-block">Nike</span> Shoes
